68f14dbd87a47b2f7091f0cb9e36691979b68ced
Michael Wang <michael_wzq@icloud.com>
2025-02-23 12:30:59 +1100

    update

--- 10-sodium/04-sodium-automation/automate.py
+++ 10-sodium/04-sodium-automation/automate.py
@@ -2,40 +2,58 @@
 
 # maximum = 2.0
 step_time = 1.0
-damping = 0.25
-folder_prefix = "el"
+damping = 1.0
+folder_prefix = "min"
+temperature = 298
 
 # ask user for input
 
-time_ns = int(input("Input time (ns): "))
+# time_ns = int(input("Input time (ns): "))
+time_ns_list = [1, 2, 5, 10, 20, 50, 100, 200, 500, 1000, 2000, 5000, 10000]
 
 # calculate relavent stuff
 import math
-
-
-print(f"{step_time=}fs")
-
-steps = int(time_ns * 1000 // step_time)
-
-print(f"{steps=}")
-
-dcd = math.ceil(steps / 200)
-
-print(f"{dcd=}")
-
-print()
-
 import os
-print("creating directory")
-os.system(f"mkdir -p '{folder_prefix}_{time_ns}'")
 
-print("copying files")
-os.system(f"cp ionized.pdb '{folder_prefix}_{time_ns}/'")
-os.system(f"cp ionized.psf '{folder_prefix}_{time_ns}/'")
-os.system(f"cp par_all27_prot_lipid.inp '{folder_prefix}_{time_ns}/'")
+for time_ns in time_ns_list:
+    print(f"{step_time=}fs")
 
-run_template=f"""#!/bin/bash
-#PBS -l walltime=12:00:00
+    steps = int(time_ns * 1000 // step_time)
+
+    print(f"{steps=}")
+
+    dcd = math.ceil(steps / 200)
+
+    print(f"{dcd=}")
+
+    walltime = math.ceil(steps / 40000)
+    print(f"Estimated {walltime}")
+    if walltime > 720:
+        print("Error: walltime exceeds allowed")
+        assert False
+    print(f"Doubling walltime")
+    walltime  *= 2
+    if walltime > 720:
+        print("Warning: double walltime exceeds allowance, cutting down")
+        walltime = 720
+    elif walltime < 10:
+        print("Info: walltime below 10 mins, extending")
+        walltime = 10
+    walltime_string = f"{walltime // 60:02d}:{walltime % 60:02d}"
+    print(walltime_string)
+    print()
+
+
+    print("creating directory")
+    os.system(f"mkdir -p '{folder_prefix}_{time_ns}'")
+
+    print("copying files")
+    os.system(f"cp ionized.pdb '{folder_prefix}_{time_ns}/'")
+    os.system(f"cp ionized.psf '{folder_prefix}_{time_ns}/'")
+    os.system(f"cp par_all27_prot_lipid.inp '{folder_prefix}_{time_ns}/'")
+
+    run_template=f"""#!/bin/bash
+#PBS -l walltime={walltime_string}:00
 #PBS -l mem=200Mb
 #PBS -l ncpus=16
 #PBS -j oe
@@ -59,16 +77,18 @@ lscpu | grep "Model name"
 PATH="/srv/scratch/z5358697/namd:$PATH" namd3 +p$(nproc) sodium.namd || qsub '{folder_prefix}_{time_ns}'
 
 # print cpuinfo
+echo hostname
 hostname
 nproc
 lscpu | grep "Model name"
+lscpu | grep "Model name" | cut -d : -f 2 | xargs
 """
 
-# os.system(f"cp run.sh '{folder_prefix}_{time_ns}/{folder_prefix}_{time_ns}'")
-with open(f"{folder_prefix}_{time_ns}/{folder_prefix}_{time_ns}", 'w') as f:
-    f.write(run_template)
+    # os.system(f"cp run.sh '{folder_prefix}_{time_ns}/{folder_prefix}_{time_ns}'")
+    with open(f"{folder_prefix}_{time_ns}/{folder_prefix}_{time_ns}", 'w') as f:
+        f.write(run_template)
 
-sodium_namd_template = f"""
+    sodium_namd_template = f"""
 #############################################################
 ## JOB DESCRIPTION                                         ##
 #############################################################
@@ -84,7 +104,6 @@ sodium_namd_template = f"""
 structure          ionized.psf
 coordinates        ionized.pdb
 
-set temperature    298
 set outputname     output
 
 firsttimestep      0
@@ -97,7 +116,7 @@ firsttimestep      0
 # Input
 paraTypeCharmm      on
 parameters          par_all27_prot_lipid.inp
-temperature         $temperature
+temperature         {temperature}
 
 
 # Force-Field Parameters
@@ -120,7 +139,7 @@ stepspercycle       10
 # Constant Temperature Control
 langevin            on   ;# do langevin dynamics
 langevinDamping     {damping}    ;# damping coefficient (gamma) of 1/ps
-langevinTemp        $temperature
+langevinTemp        {temperature}
 langevinHydrogen    off  ;# don't couple langevin bath to hydrogens
 
 # Electrostatic Force Evaluation
@@ -162,12 +181,12 @@ wrapAll             on
 #############################################################
 
 # Minimization
-minimize            100
-reinitvels          $temperature
+minimize            1000
+reinitvels          {temperature}
 
 run {steps};
 """
 
-print("writing sodium.namd")
-with open(f"{folder_prefix}_{time_ns}/sodium.namd", 'w') as f:
-    f.write(sodium_namd_template)
+    print("writing sodium.namd")
+    with open(f"{folder_prefix}_{time_ns}/sodium.namd", 'w') as f:
+        f.write(sodium_namd_template)
