165e6e89999c9c65854ad5c0250c9482373cd084
Michael Wang <michael_wzq@icloud.com>
2025-03-07 12:02:32 +1100

    update-scripts

--- 11-fep/13-historical-histagrams/histogram_simple.py
+++ 11-fep/13-historical-histagrams/histogram_simple.py
@@ -5,31 +5,50 @@ import matplotlib.mlab as mlab
 import matplotlib.pyplot as plt
 
 
-v1 = dict()
-with open("v0.1.txt") as f:
-    for line in f:
-        if line.startswith("mobley_"):
-            line = line.split(";")
-            v1[line[0].strip()] = float(line[5])
-
-v2 = dict()
-with open("v0.3.txt") as f:
-    for line in f:
-        if line.startswith("mobley_"):
-            line = line.split(";")
-            v2[line[0].strip()] = float(line[5])
-
-
-diff = list()
-for k in v1:
-    if k in v2:
-        diff.append(v2[k] - v1[k])
+# v1 = dict()
+# with open("v0.1.txt") as f:
+#     for line in f:
+#         if line.startswith("mobley_"):
+#             line = line.split(";")
+#             v1[line[0].strip()] = float(line[5])
+#
+# v2 = dict()
+# with open("v0.3.txt") as f:
+#     for line in f:
+#         if line.startswith("mobley_"):
+#             line = line.split(";")
+#             v2[line[0].strip()] = float(line[5])
+#
+#
+# diff = list()
+# for k in v1:
+#     if k in v2:
+#         diff.append(v2[k] - v1[k])
 
 # plt.title("Differences between FreeSolv v0.1 and Freesolv v0.5")
 
 plt.xlabel("Difference (kcal/mol)")
 plt.ylabel("Distribution")
-n, bins, patches = plt.hist(diff, bins=500, density=True)
+
+extra_data = """
+-0.2379118158
+0.01926301397
+-0.02040789575
+0.05720909588
+0.008913191342
+0.05676078206
+-0.03542140986
+0.04626283421
+-0.0496142232
+0.07683112459
+0.03626698281
+0.02191151369
+-0.523952676
+0.5993076624
+-0.1853533536
+""".strip().split()
+diff = list(float(i) for i in extra_data)
+n, bins, patches = plt.hist(diff, bins=25, density=True)
 bins = (bins[1:] + bins[:-1]) / 2
 # for i, j in zip(bins, n):
 #     print(f"{i},{j}")
@@ -52,7 +71,8 @@ xnew = np.linspace(min(bins), max(bins), 500)
 ynew = gauss(xnew, mu, sigma)
 l = plt.plot(xnew, ynew, "r", linewidth=1)
 plt.title(
-    r"$\mathrm{Differences\ between\ FreeSolv:}\ \mu=%.3f,\ \sigma=%.3f$" % (mu, sigma)
+    r"$\mathrm{Differences\ between\ Standard\ and\ Frozen\ Methods:}\ \mu=%.3f,\ \sigma=%.3f$"
+    % (mu, sigma)
 )
 plt.show()
 plt.show()
--- 11-fep/14-process-production/process.py
+++ 11-fep/14-process-production/process.py
@@ -6,7 +6,22 @@ import glob
 todo_list = list(
     i.split()[0].split("_")[1]
     for i in """
+mobley_2146331 formaldehyde                   chosen for aldehyde
+mobley_6091882 ethylene                       chosen for alkene
+mobley_8983100 bromomethane                   chosen for alkyl bromide
+mobley_4434915 chloromethane                  chosen for alkyl chloride
+mobley_9029594 fluoromethane                  chosen for alkyl fluoride
+mobley_4364398 iodomethane                    chosen for alkyl iodide
+mobley_525934  methanethiol                   chosen for alkylthiol; thiol (sulfanyl)
+mobley_8260524 prop-1-yne                     chosen for alkyne
+mobley_7532833 acetonitrile                   chosen for carbonitrile
+mobley_7015518 methoxymethane                 chosen for dialkyl ether
+mobley_6474572 chloroethylene                 chosen for halogen derivative
+mobley_7261305 hydrazine                      chosen for hydrazine derivative
+mobley_6714389 methanamine                    chosen for primary aliphatic amine (alkylamine); primary amine
+mobley_5692472 N-methylmethanamine            chosen for secondary aliphatic amine (dialkylamine); secondary amine
 mobley_9073553 methylsulfanylmethane          chosen for thioether
+
 """.strip().split(
         "\n"
     )
@@ -16,39 +31,39 @@ for mobley_id in todo_list:
     print(mobley_id)
     os.chdir(mobley_id)
 
-    # combine fepouts
-    os.system(
-        f"cat fw0/mobley_{mobley_id}_fw0.fepout fw1/mobley_{mobley_id}_fw1.fepout > mobley_{mobley_id}_fw.fepout"
-    )
-    os.system(
-        f"cat bw0/mobley_{mobley_id}_bw0.fepout bw1/mobley_{mobley_id}_bw1.fepout > mobley_{mobley_id}_bw.fepout"
-    )
-    os.system(
-        f"cat fwf0/mobley_{mobley_id}_fwf0.fepout fwf1/mobley_{mobley_id}_fwf1.fepout > mobley_{mobley_id}_fwf.fepout"
-    )
-    os.system(
-        f"cat bwf0/mobley_{mobley_id}_bwf0.fepout bwf1/mobley_{mobley_id}_bwf1.fepout > mobley_{mobley_id}_bwf.fepout"
-    )
+    # # combine fepouts
+    # os.system(
+    #     f"cat fw0/mobley_{mobley_id}_fw0.fepout fw1/mobley_{mobley_id}_fw1.fepout > mobley_{mobley_id}_fw.fepout"
+    # )
+    # os.system(
+    #     f"cat bw0/mobley_{mobley_id}_bw0.fepout bw1/mobley_{mobley_id}_bw1.fepout > mobley_{mobley_id}_bw.fepout"
+    # )
+    # os.system(
+    #     f"cat fwf0/mobley_{mobley_id}_fwf0.fepout fwf1/mobley_{mobley_id}_fwf1.fepout > mobley_{mobley_id}_fwf.fepout"
+    # )
+    # os.system(
+    #     f"cat bwf0/mobley_{mobley_id}_bwf0.fepout bwf1/mobley_{mobley_id}_bwf1.fepout > mobley_{mobley_id}_bwf.fepout"
+    # )
+    #
+    # # frozen
+    # subprocess.run(
+    #     ["xvfb-run", "vmd"],
+    #     input=f"parsefep -forward mobley_{mobley_id}_fwf.fepout -backward mobley_{mobley_id}_bwf.fepout -bar".encode(
+    #         "utf-8"
+    #     ),
+    # ).check_returncode()
+    # os.system(f"mv ParseFEP.log ParseFEP_frozen.log")
+    #
+    # # thawed
+    # subprocess.run(
+    #     ["xvfb-run", "vmd"],
+    #     input=f"parsefep -forward mobley_{mobley_id}_fw.fepout -backward mobley_{mobley_id}_bw.fepout -bar".encode(
+    #         "utf-8"
+    #     ),
+    # ).check_returncode()
 
-    # frozen
-    subprocess.run(
-        ["xvfb-run", "vmd"],
-        input=f"parsefep -forward mobley_{mobley_id}_fwf.fepout -backward mobley_{mobley_id}_bwf.fepout -bar".encode(
-            "utf-8"
-        ),
-    ).check_returncode()
-    os.system(f"mv ParseFEP.log ParseFEP_frozen.log")
-
-    # thawed
-    subprocess.run(
-        ["xvfb-run", "vmd"],
-        input=f"parsefep -forward mobley_{mobley_id}_fw.fepout -backward mobley_{mobley_id}_bw.fepout -bar".encode(
-            "utf-8"
-        ),
-    ).check_returncode()
-
-    os.system("tail -n 1 ParseFEP.log")
-    os.system("tail -n 1 ParseFEP_frozen.log")
+    os.system("tail -n 1 ParseFEP.log | cut -d ' ' -f 7")
+    os.system("tail -n 1 ParseFEP_frozen.log | cut -d ' ' -f 7")
 
     for mode in ["fw0", "fw1", "bw0", "bw1", "fwf0", "fwf1", "bwf0", "bwf1"]:
         os.chdir(mode)
@@ -81,23 +96,23 @@ for mobley_id in todo_list:
         memory = resources[8]
 
         # get fep
-        fep = (
-            subprocess.run(
-                [
-                    "grep",
-                    "-F",
-                    "net change until now is",
-                    f"mobley_{mobley_id}_{mode}.fepout",
-                ],
-                capture_output=True,
-            )
-            .stdout.decode("utf-8")
-            .strip()
-            .split("\n")[-1]
-            .split()[-1]
-        )
+        # fep = (
+        #     subprocess.run(
+        #         [
+        #             "grep",
+        #             "-F",
+        #             "net change until now is",
+        #             f"mobley_{mobley_id}_{mode}.fepout",
+        #         ],
+        #         capture_output=True,
+        #     )
+        #     .stdout.decode("utf-8")
+        #     .strip()
+        #     .split("\n")[-1]
+        #     .split()[-1]
+        # )
 
-        print(f"{fep},{hostname},{walltime},{memory}")
+        # print(f"{fep},{hostname},{walltime},{memory}")
 
         os.chdir("..")
     os.chdir("..")
