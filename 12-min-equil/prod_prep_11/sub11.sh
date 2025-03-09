b6e004930050adecd4358f936e90831f9938de8b
Michael Wang <michael_wzq@icloud.com>
2025-03-09 18:43:59 +1100

    update-12-11

---
+++ 12-min-equil/prod_prep_11/sub11.sh
@@ -0,0 +1,150 @@
+#!/usr/bin/bash
+set -e
+cd 8522124; qsub 8522124_prep; cd .. 
+#!/usr/bin/bash
+set -e
+cd 8525830; qsub 8525830_prep; cd .. 
+#!/usr/bin/bash
+set -e
+cd 852937; qsub 852937_prep; cd .. 
+#!/usr/bin/bash
+set -e
+cd 8558116; qsub 8558116_prep; cd .. 
+#!/usr/bin/bash
+set -e
+cd 8573194; qsub 8573194_prep; cd .. 
+#!/usr/bin/bash
+set -e
+cd 8578590; qsub 8578590_prep; cd .. 
+#!/usr/bin/bash
+set -e
+cd 859464; qsub 859464_prep; cd .. 
+#!/usr/bin/bash
+set -e
+cd 8614858; qsub 8614858_prep; cd .. 
+#!/usr/bin/bash
+set -e
+cd 8668219; qsub 8668219_prep; cd .. 
+#!/usr/bin/bash
+set -e
+cd 8685905; qsub 8685905_prep; cd .. 
+#!/usr/bin/bash
+set -e
+cd 8691603; qsub 8691603_prep; cd .. 
+#!/usr/bin/bash
+set -e
+cd 8705848; qsub 8705848_prep; cd .. 
+#!/usr/bin/bash
+set -e
+cd 8713762; qsub 8713762_prep; cd .. 
+#!/usr/bin/bash
+set -e
+cd 8723116; qsub 8723116_prep; cd .. 
+#!/usr/bin/bash
+set -e
+cd 8739734; qsub 8739734_prep; cd .. 
+#!/usr/bin/bash
+set -e
+cd 8746821; qsub 8746821_prep; cd .. 
+#!/usr/bin/bash
+set -e
+cd 8754702; qsub 8754702_prep; cd .. 
+#!/usr/bin/bash
+set -e
+cd 8764620; qsub 8764620_prep; cd .. 
+#!/usr/bin/bash
+set -e
+cd 8765203; qsub 8765203_prep; cd .. 
+#!/usr/bin/bash
+set -e
+cd 8772587; qsub 8772587_prep; cd .. 
+#!/usr/bin/bash
+set -e
+cd 8785107; qsub 8785107_prep; cd .. 
+#!/usr/bin/bash
+set -e
+cd 8789465; qsub 8789465_prep; cd .. 
+#!/usr/bin/bash
+set -e
+cd 8789864; qsub 8789864_prep; cd .. 
+#!/usr/bin/bash
+set -e
+cd 8798016; qsub 8798016_prep; cd .. 
+#!/usr/bin/bash
+set -e
+cd 8809190; qsub 8809190_prep; cd .. 
+#!/usr/bin/bash
+set -e
+cd 8809274; qsub 8809274_prep; cd .. 
+#!/usr/bin/bash
+set -e
+cd 8823527; qsub 8823527_prep; cd .. 
+#!/usr/bin/bash
+set -e
+cd 8827942; qsub 8827942_prep; cd .. 
+#!/usr/bin/bash
+set -e
+cd 8861672; qsub 8861672_prep; cd .. 
+#!/usr/bin/bash
+set -e
+cd 8882696; qsub 8882696_prep; cd .. 
+#!/usr/bin/bash
+set -e
+cd 8883511; qsub 8883511_prep; cd .. 
+#!/usr/bin/bash
+set -e
+cd 8885088; qsub 8885088_prep; cd .. 
+#!/usr/bin/bash
+set -e
+cd 8899867; qsub 8899867_prep; cd .. 
+#!/usr/bin/bash
+set -e
+cd 8916409; qsub 8916409_prep; cd .. 
+#!/usr/bin/bash
+set -e
+cd 8966374; qsub 8966374_prep; cd .. 
+#!/usr/bin/bash
+set -e
+cd 8983100; qsub 8983100_prep; cd .. 
+#!/usr/bin/bash
+set -e
+cd 900088; qsub 900088_prep; cd .. 
+#!/usr/bin/bash
+set -e
+cd 9007496; qsub 9007496_prep; cd .. 
+#!/usr/bin/bash
+set -e
+cd 9015240; qsub 9015240_prep; cd .. 
+#!/usr/bin/bash
+set -e
+cd 9028462; qsub 9028462_prep; cd .. 
+#!/usr/bin/bash
+set -e
+cd 902954; qsub 902954_prep; cd .. 
+#!/usr/bin/bash
+set -e
+cd 9029594; qsub 9029594_prep; cd .. 
+#!/usr/bin/bash
+set -e
+cd 9055303; qsub 9055303_prep; cd .. 
+#!/usr/bin/bash
+set -e
+cd 9073553; qsub 9073553_prep; cd .. 
+#!/usr/bin/bash
+set -e
+cd 9100956; qsub 9100956_prep; cd .. 
+#!/usr/bin/bash
+set -e
+cd 9112978; qsub 9112978_prep; cd .. 
+#!/usr/bin/bash
+set -e
+cd 9114381; qsub 9114381_prep; cd .. 
+#!/usr/bin/bash
+set -e
+cd 9121449; qsub 9121449_prep; cd .. 
+#!/usr/bin/bash
+set -e
+cd 9139060; qsub 9139060_prep; cd .. 
+#!/usr/bin/bash
+set -e
+cd 9185328; qsub 9185328_prep; cd .. 
