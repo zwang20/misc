88e37919e0833022a7bd06b0987f627c16604424
Michael Wang <michael_wzq@icloud.com>
2025-03-10 17:18:34 +1100

    update-13

---
+++ 13-qm/02-gen-input/gen-com.py
@@ -0,0 +1,34 @@
+#!/usr/bin/python
+import os
+import subprocess
+
+prefix_list = list(
+    i.split()[0].split("_")[1]
+    for i in """
+mobley_2146331
+mobley_6091882
+mobley_8983100
+mobley_4434915
+mobley_9029594
+mobley_4364398
+mobley_525934
+mobley_8260524
+mobley_7532833
+mobley_7015518
+mobley_6474572
+mobley_7261305
+mobley_6714389
+mobley_5692472
+mobley_9073553
+""".strip().split(
+        "\n"
+    )
+)
+
+
+for prefix in prefix_list:
+    print(prefix)
+    subprocess.run(
+        ["obabel", f"FreeSolv/mol2files_sybyl/mobley_{prefix}.mol2", f"-O{prefix}.com"]
+    )
+    assert False
