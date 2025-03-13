a8401057117f7f27d3a26f0133abe26cb0a28c2e
Michael Wang <michael_wzq@icloud.com>
2025-03-13 15:39:11 +1100

    14-prod/common.py

--- 14-prod/common.py
+++ 14-prod/common.py
@@ -1,4 +1,18 @@
 import subprocess
 
+
 def get_previous_files():
-    return subprocess.run(["ls", "batch"], capture_output=True, check=True.stdout.decode("utf-8").strip().split()
+    return (
+        subprocess.run(["ls", "batch"], capture_output=True, check=True)
+        .stdout.decode("utf-8")
+        .strip()
+        .split()
+    )
+
+
+def get_prefix_list(batch):
+    prefix_list = []
+    with open(f"batch/{batch}.txt", encoding="utf-8") as f:
+        for line in f:
+            prefix_list.append(line.strip())
+    return prefix_list
