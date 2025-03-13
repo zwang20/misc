185e494d66d386e3919be8a2be28ef3db3523fb7
Michael Wang <michael_wzq@icloud.com>
2025-03-13 12:40:21 +1100

    git-push.py

---
+++ git-push.py
@@ -0,0 +1,33 @@
+#!/usr/bin/python
+
+import subprocess
+import time
+
+MAX_SIZE = 104857600
+
+
+def get_files():
+
+    output = []
+
+    git_process = subprocess.run(["git", "status", "-u"], capture_output=True)
+
+    print(git_process.stderr.decode("utf-8"))
+    git_process.check_returncode()
+
+    for line in git_process.stdout.decode("utf-8").split("\n"):
+        if line.startswith("\t") and not line.endswith("/") and not line.endswith(")"):
+            if ":" in line:
+                output.append(line.split(maxsplit=1)[1])
+            else:
+                output.append(line.strip())
+
+    return output
+
+
+for filename in get_files():
+    print(filename)
+    subprocess.run(["git", "add", filename]).check_returncode()
+    subprocess.run(["git", "commit", "-m", filename]).check_returncode()
+    subprocess.run(["git", "push"]).check_returncode()
+    time.sleep(10)
