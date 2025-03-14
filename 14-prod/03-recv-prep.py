591d1dcfbc5c8b7bf1581bcde4f6a413055dbbc4
Michael Wang <michael_wzq@icloud.com>
2025-03-14 14:19:07 +1100

    14-prod/03-recv-prep.py

--- 14-prod/03-recv-prep.py
+++ 14-prod/03-recv-prep.py
@@ -5,4 +5,4 @@ import os
 from common import get_batch_number
 
 print(batch := get_batch_number())
-os.system(f"scp -r kdm:/srv/scratch/z5358697/aa-prep/{batch} aa-prep/")
+os.system(f"rsync -rP kdm:/srv/scratch/z5358697/aa-prep/ aa-prep/")
