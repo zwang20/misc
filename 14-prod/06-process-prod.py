012a96c9c5000af22630b641b7f3eabcef054abc
Michael Wang <michael_wzq@icloud.com>
2025-03-17 11:03:15 +1100

    14-prod/06-process-prod.py

---
+++ 14-prod/06-process-prod.py
@@ -0,0 +1,17 @@
+"""
+process-prod.py
+
+processes results of production run
+"""
+
+from common import get_prefix_list, get_batch_number
+
+batch_number = get_batch_number()
+prefix_list = get_prefix_list(batch_number)
+
+# Things to extract
+# forward
+# backward
+# bar
+
+# get the forward
