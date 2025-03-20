bf11c93b862081548eae1a62a8dcfe47dd4a2c3e
Michael Wang <michael_wzq@icloud.com>
2025-03-20 22:24:10 +1100

    15-plot/print.py

---
+++ 15-plot/print.py
@@ -0,0 +1,36 @@
+#!/usr/bin/python
+
+
+import rdkit.Chem  # type: ignore[import-untyped]
+import rdkit.Chem.Lipinski  # type: ignore[import-untyped]
+
+max_rot = 0
+max_atom = 0
+
+with open("database.txt", encoding="utf-8") as f:
+    for line in f:
+        if line.startswith("#"):
+            continue
+        line_list = line.split(";")
+        prefix = line_list[0].strip().split("_")[1]
+
+        smiles = line_list[1].strip()
+        iupac = line_list[2].strip()
+
+        mol = rdkit.Chem.rdmolfiles.MolFromSmiles(smiles)
+        num_rotatable_bonds = rdkit.Chem.Lipinski.NumRotatableBonds(mol)
+        mol = rdkit.Chem.AddHs(mol)
+        num_atoms = len(mol.GetAtoms())
+
+        # if num_rotatable_bonds > max_rot:
+        #     print(num_rotatable_bonds, num_atoms, prefix, iupac)
+        #     max_rot = num_rotatable_bonds
+        #     max_atom = num_atoms
+        #
+        # elif num_rotatable_bonds == max_rot and num_atoms > max_atom:
+        #     print(num_rotatable_bonds, num_atoms, prefix, iupac)
+        #     max_rot = num_rotatable_bonds
+        #     max_atom = num_atoms
+
+        if num_atoms >= 40:
+            print(num_rotatable_bonds, num_atoms, prefix, iupac)
