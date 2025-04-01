57ed956a2fc3bcd5df2bb87a6ef322e6166e9ee3
Michael Wang <michael_wzq@icloud.com>
2025-04-01 12:46:05 +1100

    17-rir/frozen-solute-model/crest.py

---
+++ 17-rir/frozen-solute-model/crest.py
@@ -0,0 +1,10 @@
+import sys
+
+from common import katana_crest
+
+assert len(sys.argv) == 3, (sys.argv, len(sys.argv))
+assert sys.argv[1].startswith("data/"), sys.argv[1]
+assert "_" in sys.argv[2], sys.argv[2]
+
+with open(f"{sys.argv[1]}", "w") as f:
+    f.write(katana_crest.format(mobley_id=sys.argv[2]))
