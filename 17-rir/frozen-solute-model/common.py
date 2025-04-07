198c384b26d2e5b65608a4765d14b8f8e5819bdd
Michael Wang <michael_wzq@icloud.com>
2025-04-07 13:12:01 +1000

    17-rir/frozen-solute-model/common.py

--- 17-rir/frozen-solute-model/common.py
+++ 17-rir/frozen-solute-model/common.py
@@ -275,6 +275,23 @@ module add crest
 crest {mobley_id}.xyz --alpb water --chrg 0 --uhf 0 -T 16 --noreftopo > crest.log
 """
 
+katana_vacuum_crest = """#!/usr/bin/bash
+#PBS -l walltime=12:00:00
+#PBS -l mem=16Gb
+#PBS -l ncpus=16
+#PBS -l select=cpuflags=avx512_vpopcntdq
+#PBS -j oe
+set -e
+
+lscpu
+
+cd "${{PBS_O_WORKDIR}}"
+
+module add crest
+
+crest {mobley_id}.xyz --chrg 0 --uhf 0 -T 16 --noreftopo > crest.log
+"""
+
 katana_censo = """#!/usr/bin/bash
 #PBS -l walltime=12:00:00
 #PBS -l mem={mem}Gb
