f1aa720815a4466e63c48e885576071889df07aa
Michael Wang <michael_wzq@icloud.com>
2025-04-08 13:47:49 +1000

    git-push.py

--- git-push.py
+++ git-push.py
@@ -9,27 +9,44 @@ MAX_SIZE = 104857600
 def get_files():
 
     output = []
+    deleted = []
 
-    git_process = subprocess.run(["git", "status", "-u"], capture_output=True)
+    git_process = subprocess.run(
+        ["git", "status", "-u"], capture_output=True, check=True
+    )
 
     print(git_process.stderr.decode("utf-8"))
-    git_process.check_returncode()
 
     for line in git_process.stdout.decode("utf-8").split("\n"):
         if line.startswith("\t") and not line.endswith("/") and not line.endswith(")"):
-            if ":" in line:
+            if "deleted:" in line:
+                deleted.append(line.split(maxsplit=1)[1])
+            elif ":" in line:
                 output.append(line.split(maxsplit=1)[1])
             else:
                 output.append(line.strip())
 
-    return output
+    return deleted, output
 
 
-subprocess.run(['git', 'pull'], check=True)
+def chunks(lst, n):
+    """Yield successive n-sized chunks from lst."""
+    for i in range(0, len(lst), n):
+        yield lst[i : i + n]
 
-for filename in get_files():
-    print(filename)
-    subprocess.run(["git", "add", filename]).check_returncode()
-    subprocess.run(["git", "commit", "-m", filename]).check_returncode()
+
+subprocess.run(["git", "pull"], check=True)
+
+deleted, output = get_files()
+
+for files in chunks(deleted, 128):
+    subprocess.run(["git", "add"] + files, check=True)
+    subprocess.run(["git", "commit", "-m", "remove files"]).check_returncode()
+    subprocess.run(["git", "push"]).check_returncode()
+    time.sleep(10)
+
+for files in chunks(output, 8):
+    subprocess.run(["git", "add"] + files, check=True)
+    subprocess.run(["git", "commit", "-m", "add files"]).check_returncode()
     subprocess.run(["git", "push"]).check_returncode()
     time.sleep(10)
