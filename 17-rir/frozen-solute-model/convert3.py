a17140e1570bcb4ce5e2029c13bca1b0f06131d5
Michael Wang <michael_wzq@icloud.com>
2025-04-11 11:19:53 +1000

    add files

--- 17-rir/frozen-solute-model/convert3.py
+++ 17-rir/frozen-solute-model/convert3.py
@@ -10,3 +10,10 @@ import subprocess
 
 con = sqlite3.connect("frozen_solute_model_new.db")
 cur = con.cursor()
+
+censo_paths = cur.execute(
+    "SELECT local_path FROM runs WHERE run_type = 'CENSO'"
+).fetchall()
+
+for i in censo_paths:
+    censo_paths = i[0]
